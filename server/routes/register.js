40fe60a0585554c39a8cbd445a2a01e8d4f6f551
Alex Mayorga <ninjas@3menmovers.com>
2020-07-03 11:03:07 -0500

    Updated the query to check for existing 'username' and 'email'

--- server/routes/register.js
+++ server/routes/register.js
@@ -17,54 +17,54 @@ router.get('/', function (req, res, next) {
 
 
 router.post('/', function (req, res, next) {
+    if (!req.body.email) return res.status(404).end('Nothing send')
 
     const user = new User(req.body);
 
-    User.findOne({email: user.email},(err,emailDocs)=>{
-        if (err) return res.status(404).end();
-        if(emailDocs) return res.status(404).json({message: "Email is already in use"});
+    User.findOne({
+        $or: [
+            { email: req.body.email },
+            { username: req.body.username }
+        ]
+    }).exec(function (err, userDoc) {
 
-        User.findOne({ username: user.username }, (err, userNameDocs) => {
+        if (userDoc && (userDoc.email === req.body.email)) return res.status(404).json({message: "Email is already in use"}) 
+        if (userDoc && (userDoc.username === req.body.username)) return res.status(404).json({message: "Username is already in use"}) 
+
+        user.genRefreshToken((err, user, refreshToken, accessToken) => {
             if (err) return res.status(404).end();
-            if (userNameDocs) return res.status(404).json({ message: "Username is already in use" });
 
-            user.genRefreshToken((err, user, refreshToken, accessToken) => {
-                if (err) return res.status(404).end();
-
-                const msg = {
-                    to: `${user.email}`,
-                    from: 'Mayorga Dev <me@alexmayorga.dev>',
-                    subject: 'Verification Email',
-                    text: `Hi ${user.firstname}, Please use this link to verify your email address: http://localhost:3000/verify-account/${user.email}/${refreshToken}`,
-                    html: `
+            const msg = {
+                to: `${user.email}`,
+                from: 'Mayorga Dev <me@alexmayorga.dev>',
+                subject: 'Verification Email',
+                text: `Hi ${user.firstname}, Please use this link to verify your email address: http://localhost:3000/verify-account/${user.email}/${refreshToken}`,
+                html: `
                 Hi ${user.firstname}, Please use this link to verify your email address:
                 <br><br>
                 http://localhost:3000/verify-account/${user.email}/${refreshToken}
             `,
-                };
-                //ES6
-                sgMail
-                    .send(msg)
-                    .then(() => {
-                        // console.log("Message sent!");
-                    }, error => {
-                        console.error(error);
+            };
+            //ES6
+            sgMail
+                .send(msg)
+                .then(() => {
+                    // console.log("Message sent!");
+                }, error => {
+                    console.error(error);
 
-                        if (error.response) return console.error(error.response.body)
+                    if (error.response) return console.error(error.response.body)
 
-                    });
+                });
 
 
-                res
-                    .cookie('refreshToken', refreshToken, { httpOnly: true, expires: new Date(Date.now() + 24 * 3600000) })
-                    .cookie('accessToken', accessToken, { httpOnly: true, expires: new Date(Date.now() + (60 * 10000)) })
-                    .status(200)
-                    .end();
-            })
-
+            res
+                .cookie('refreshToken', refreshToken, { httpOnly: true, expires: new Date(Date.now() + 24 * 3600000) })
+                .cookie('accessToken', accessToken, { httpOnly: true, expires: new Date(Date.now() + (60 * 10000)) })
+                .status(200)
+                .end();
         })
-
-    })
+    });
 
 });
 
